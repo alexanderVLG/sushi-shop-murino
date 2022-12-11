24887ea0790dba0e9f412a7adc27d6ca589aea55
alexanderVLG <dominorog323@gmail.com>
2022-12-11 21:57:46 +0300

    оформление карточки товара

--- src/pages/ProductCard.js
+++ src/pages/ProductCard.js
@@ -12,21 +12,23 @@ const ProductCard = () => {
       <section className='product-card__container'>
         <div className='link-arrow__wrapper'>
           <ul>
-            <li>Назад</li>
-            <li>Вперед</li>
+            <li><span></span>Назад</li>
+            <li>Вперед<span></span></li>
           </ul>
         </div>
-        <article className='product-card'>
-          <h4 className='product-card__title'>{name}</h4>
-          <img src={cardImage} alt={name} />
-          <p className='product-card__desc'>{product.weight} грамм, {product.pieces} кусочков</p>
-          <p className='product-card__price'>{product.cost} РУБ</p>
-          <button className='product-card__btn' type='button'>Хочу!</button>
-        </article>
-        <article>
-          <h5>Состав сета</h5>
-          <GoodsCarousel />
-        </article>
+        <section className='product-card'>
+          <article>
+            <h4 className='product-card__title'>{name}</h4>
+            <img src={cardImage} alt={name} />
+            <p className='product-card__desc'>{product.weight} грамм, {product.pieces} кусочков</p>
+            <p className='product-card__price'>{product.cost} руб</p>
+            <button className='product-card__btn' type='button'>Хочу!</button>
+          </article>
+          <article>
+            <h5>Состав сета</h5>
+            <GoodsCarousel />
+          </article>
+        </section>
       </section>
       <About />
     </>    
